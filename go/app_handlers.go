898be2d8cd382937b538e17514107ee9be8b8fa4
tamaron1820 <tatsuhiko02182002@gmail.com>
2025-01-07 17:50:29 +0900

    fix: 🐛 微修正

--- go/app_handlers.go
+++ go/app_handlers.go
@@ -884,10 +884,6 @@ func appGetNearbyChairs(w http.ResponseWriter, r *http.Request) {
 
 	nearbyChairs := []appGetNearbyChairsResponseChair{}
 	for _, chair := range chairs {
-		if !chair.IsActive {
-			continue
-		}
-
 		rides := []*Ride{}
 		if err := tx.SelectContext(ctx, &rides, `SELECT * FROM rides WHERE chair_id = ? ORDER BY created_at DESC LIMIT 1`, chair.ID); err != nil {
 			writeError(w, http.StatusInternalServerError, err)
