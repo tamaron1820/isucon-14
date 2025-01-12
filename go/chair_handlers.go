853dd037b93571c608d37dc9a7d1b921dd855ebc
tamaron1820 <tatsuhiko02182002@gmail.com>
2025-01-12 23:18:19 +0900

    fix: 🐛 通知を短く

--- go/chair_handlers.go
+++ go/chair_handlers.go
@@ -227,7 +227,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id = ? ORDER BY updated_at DESC LIMIT 1`, chair.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &chairGetNotificationResponse{
-				RetryAfterMs: 500,
+				RetryAfterMs: 100,
 			})
 			return
 		}
@@ -287,7 +287,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 			},
 			Status: status,
 		},
-		RetryAfterMs: 500,
+		RetryAfterMs: 100,
 	})
 }
 
